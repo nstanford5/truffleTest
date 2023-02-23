76f9723baf02c5251045f5f81a22dac0c4ca33e8
Nick Stanford <nick@reach.sh>
2023-02-22 17:30:09 -0700

    simplify

--- contracts/myERC.sol
+++ contracts/myERC.sol
@@ -3,20 +3,17 @@ pragma solidity >=0.4.25 <0.9.0;
 import "../node_modules/@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Address.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
-// import "../node_modules/@openzeppelin/contracts/utils/Context.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
-contract myERC is ERC165, Ownable, IERC721, IERC721Metadata {
+contract myERC is ERC165, Ownable, IERC721 {
   using Address for address;
   using Strings for uint256;
   using Counters for Counters.Counter;
 
   Counters.Counter private _tokenIdCounter;
-
   string private _name;
   string private _symbol;
 
@@ -31,73 +28,52 @@ contract myERC is ERC165, Ownable, IERC721, IERC721Metadata {
     _symbol = symbol_;
   }
 
-  // tested, works
-  function name() public view virtual override returns (string memory) {
+  // helper
+  // tested
+  function name() public view virtual returns (string memory) {
     return _name;
   }
 
-  // tested, works
-  function symbol() public view virtual override returns (string memory) {
+  // tested
+  // helper
+  function symbol() public view virtual returns (string memory) {
     return _symbol;
   }
 
-  // need to replace this with IPFS URI
-  function _baseURI() internal view virtual returns (string memory) {
-    return "test";
-  }
-
-  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-    _requireMinted(tokenId);
-
-    string memory baseURI = _baseURI();
-    return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
-  }
-
-  // tested, works
-  function _requireMinted(uint256 tokenId) internal view virtual {
-    require(_exists(tokenId), "ERC721: invalid token ID");
-  }
-
+  // required by spec, check that it supports ERC165 
   function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
     return 
       interfaceId == type(IERC721).interfaceId ||
-      interfaceId == type(IERC721Metadata).interfaceId ||
       super.supportsInterface(interfaceId);
   }
-  // tested, this works
-  // should I add the ability to only have the owner be able to mint?
-  function safeMint(address to) public {
-    uint256 tokenId = _tokenIdCounter.current();
-    _tokenIdCounter.increment();
-    _safeMint(to, tokenId);
-  }
 
+  // required by spec
+  // count all NFTs assigned to an owner
   function balanceOf(address owner) external view returns (uint256){
     require(owner != address(0), "ERC721: address zero is not a valid owner");
     return _balances[owner];
   }
 
-  function ownerOf(uint256 tokenId) public view virtual override returns (address) {
-    address owner = _ownerOf(tokenId);
+  // required by spec
+  // find the owner of an NFT
+  function ownerOf(uint256 tokenId) public view returns (address) {
+    address owner = _owners[tokenId];
     require(owner != address(0), "ERC721: invalid token ID");
     return owner;
   }
 
-  function _safeMint(address to, uint256 tokenId) internal virtual {
-    _safeMint(to, tokenId, "");
-  }
-  
-  function _safeMint(address to, uint256 tokenId, bytes memory data) internal virtual {
-    _mint(to, tokenId);
+  // extra
+  function mint(address to) public {
+    require(to != address(0), "ERC721: mint to the zero address");
+
+    uint256 tokenId = _tokenIdCounter.current();
+    require(_owners[tokenId] == address(0), "ERC721: token already minted");
+    _tokenIdCounter.increment();
+
     require(
-      _checkOnERC721Received(address(0), to, tokenId, data),
+      _checkOnERC721Received(address(0), to, tokenId, ""),
       "ERC721: transfer to non ERC721Receiver implementer"
     );
-  }
-
-  function _mint(address to, uint256 tokenId) internal virtual {
-    require(to != address(0), "ERC721: mint to the zero address");
-    require(!_exists(tokenId), "ERC721: token already minted");
 
     unchecked {
       _balances[to] += 1;
@@ -105,31 +81,13 @@ contract myERC is ERC165, Ownable, IERC721, IERC721Metadata {
 
     _owners[tokenId] = to;
 
+    // required by spec
     emit Transfer(address(0), to, tokenId);
-
   }
-
-  // function _burn(uint256 tokenId) internal virtual {
-  //   address owner = myERC.ownerOf(tokenId);
-
-  //   _beforeTokenTransfer(owner, address(0), tokenId, 1);
-
-  //   owner = myERC.ownerOf(tokenId);
-  //   delete _tokenApprovals[tokenId];
-  //   unchecked {
-  //     _balances[owner] -= 1;
-  //   }
-  //   delete _owners[tokenId];
-  //   emit Transfer(owner, address(0), tokenId);
-
-  //   _afterTokenTransfer(owner, address(0), tokenId, 1);
-  // }
 
   function transfer(address from, address to, uint256 tokenId) public {
     require(myERC.ownerOf(tokenId) == from, "ERC721: transfer from incorrect owner");
     require(to != address(0), "ERC721: transfer to the zero address");
-
-    require(myERC.ownerOf(tokenId) == from, "ERC721: transfer from incorrect owner");
 
     delete _tokenApprovals[tokenId];
 
@@ -140,78 +98,71 @@ contract myERC is ERC165, Ownable, IERC721, IERC721Metadata {
 
     _owners[tokenId] = to;
 
+    // the ERC721 standard requires we emit this event
     emit Transfer(from, to, tokenId);
   }
 
-  // comment this out and you get back to the "should be abstract" error
-  function approve(address to, uint256 tokenId) public virtual override {
+  // required by the spec (marked as external payable)
+  function approve(address to, uint256 tokenId) external {
     address owner = myERC.ownerOf(tokenId);
     require(to != owner, "ERC721: approval to current owner");
 
+    // check that the caller has the authority to approve addresses
     require(
       _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
       "ERC721: approved caller is not token owner or approved for all"
     );
-  }
 
-  function _approve(address to, uint256 tokenId) internal virtual {
     _tokenApprovals[tokenId] = to;
-    emit Approval(myERC.ownerOf(tokenId), to, tokenId);
+
+    // the ERC721 standard requires we emit this event when a new approval occurs
+    emit Approval(owner, to, tokenId);
   }
 
-  function _setApprovalForAll(address owner, address operator, bool approved) internal virtual {
-    require(owner != operator, "ERC721: approve to caller");
-    _operatorApprovals[owner][operator] = approved;
-    emit ApprovalForAll(owner, operator, approved);
-  }
-
-  function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
+  // helper
+  function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
     address owner = myERC.ownerOf(tokenId);
     return (spender == owner || isApprovedForAll(owner, spender) || getApproved(tokenId) == spender);
   }
 
-  function getApproved(uint256 tokenId) public view virtual override returns (address) {
-    _requireMinted(tokenId);
+  // required by the spec
+  function getApproved(uint256 tokenId) public view returns (address) {
+    require(_owners[tokenId] != address(0), "ERC721: invalid token ID");
     return _tokenApprovals[tokenId];
   }
 
-  function isApprovedForAll(address owner, address operator) public view virtual override returns (bool) {
+  // required by the spec
+  function isApprovedForAll(address owner, address operator) public view returns (bool) {
     return _operatorApprovals[owner][operator];
   }
 
-  function setApprovalForAll(address operator, bool approved) public virtual override {
-    _setApprovalForAll(_msgSender(), operator, approved);
+  // required by spec
+  function setApprovalForAll(address operator, bool approved) external {
+    require(_msgSender() != operator, "ERC721: approve to caller");
+    _operatorApprovals[_msgSender()][operator] = approved;
+    emit ApprovalForAll(_msgSender(), operator, approved);
   }
 
-  function transferFrom(address from, address to, uint256 tokenId) public virtual override {
+  // required by the spec
+  function transferFrom(address from, address to, uint256 tokenId) external {
     require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
-
     transfer(from, to, tokenId);
   }
 
-  function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
+  // required by the spec
+  function safeTransferFrom(address from, address to, uint256 tokenId) external {
     safeTransferFrom(from, to, tokenId, "");
   }
 
-  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public virtual override {
+  // required by the spec
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public {
     require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
-    _safeTransfer(from, to, tokenId, data);
-  }
-
-  function _safeTransfer(address from, address to, uint256 tokenId, bytes memory data) internal virtual {
     transfer(from, to, tokenId);
-    require(_checkOnERC721Received(from, to, tokenId, data), "ERC721: transfer to non ERC721Receiver implementer");
+    require(_checkOnERC721Received(from, to, tokenId, data), "ERC721: transfer to non ERC721REceiver implementer");
   }
 
-  function _ownerOf(uint256 tokenId) internal view virtual returns (address) {
-    return _owners[tokenId];
-  }
-
-  function _exists(uint256 tokenId) internal view virtual returns (bool) {
-      return _ownerOf(tokenId) != address(0);
-  }
-
-  // all ERC721 tokens must implement the ERC721Receiver
+  // required by spec
+  // handle the receipt of the NFT
   function _checkOnERC721Received(
     address from,
     address to,
